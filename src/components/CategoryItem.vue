6113ea6e595a49312e65f20477b1d8960cbe879b
danelian <141317269+danelian@users.noreply.github.com>
2023-10-25 04:36:10 +0300

    4: Create category component

---
+++ src/components/CategoryItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <a
+    href="#"
+    :class="classes"
+  >
+    {{ category }}
+  </a>
+</template>
+
+<script>
+export default {
+  props: ['category', 'isActive'],
+
+  computed: {
+    classes() {
+      const classes = [
+        'px-3', 'py-1', 'transition', 'border', 'rounded-full',
+      ]
+
+      return this.isActive 
+        ? [
+          ...classes,
+          'bg-gray-600',
+          'border-gray-700', 
+          'hover:bg-gray-500', 
+          'text-white'
+        ]
+        : [ ...classes, 'border-gray-300', 'bg-gray-100', 'hover:bg-gray-200']
+    }
+  }
+}
+</script>
--- src/components/TheCategories.vue
+++ src/components/TheCategories.vue
@@ -4,21 +4,24 @@
   >
     <div class="border-t border-b px-4 max-w-screen-2xl m-auto">
       <div class="py-3 flex space-x-3 overflow-auto text-sm whitespace-nowrap">
-        <a
-          v-for="category in categories"
-          :key="category"
-          href="#"
-          class="px-3 py-1 transition bg-gray-600 border border-gray-700 rounded-full hover:bg-gray-500 text-white"
-          >
-          {{ category }}
-        </a>
+        <CategoryItem 
+          v-for="category in categories" 
+          :key="category" 
+          :category="category"
+          :is-active="category === 'All'"
+        />
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import CategoryItem from './CategoryItem.vue';
+
 export default {
+  components: {
+    CategoryItem,
+  },
   data() {
     return {
       categories: [
