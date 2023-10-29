a2d7c124737ad2304af714fe35865ccc2aa44247
danelian <141317269+danelian@users.noreply.github.com>
2023-10-29 05:08:01 +0300

    20: Animate video buttons

--- src/components/VideoItemThumbnail.vue
+++ src/components/VideoItemThumbnail.vue
@@ -1,16 +1,8 @@
 <template>
   <div class="relative">
     <img :src="`/channel-${index}.jpg`" />
-    <span
-      class="opacity-0 group-hover:opacity-100 bg-opacity-60 absolute top-0 right-0 bg-black text-white rounded-sm m-1 p-1"
-    >
-      <BaseIcon name="clock" class="w-5 h-5" />
-    </span>
-    <span
-      class="opacity-0 group-hover:opacity-100 bg-opacity-60 absolute top-8 right-0 bg-black text-white rounded-sm m-1 p-1"
-    >
-      <BaseIcon name="menuAlt3" class="w-5 h-5" />
-    </span>
+    <VideoItemThumbnailButton icon="clock" label="Whatch later" class="top-0" />
+    <VideoItemThumbnailButton icon="menuAlt3" label="Add to queue" class="top-8" />
     <span
       class="opacity-100 group-hover:opacity-0 duration-500 absolute bottom-0 right-0 bg-black text-white rounded-sm m-1 p-1 text-xs font-semibold"
       >6:35</span
@@ -20,10 +12,12 @@
 
 <script>
 import BaseIcon from './BaseIcon.vue';
+import VideoItemThumbnailButton from './VideoItemThumbnailButton.vue';
 
 export default {
   components: {
-    BaseIcon
+    BaseIcon,
+    VideoItemThumbnailButton
   },
 
   props: ['index']
